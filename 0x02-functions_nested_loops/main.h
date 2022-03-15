4562e9feabe00553917861ff23abc5dfa7583564
Liz Odhiambo <elizabethodhiambo418@gmail.com>
2022-03-15 23:38:35 +0300

    Added main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,19 @@
-#ifndef MY_HEADER
-#define MY_HEADER
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * This contains all the functions that will be used in this program
+ */
 
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _putchar(char c);
 int _islower(int c);
-int _isalpha(int c);
+int _isalpha(int C);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-void print_to_98(int n);int add(int, int);
-void print_times_table(int n);
+int add(int, int);
+void print_to_98(int n);
+#endif
